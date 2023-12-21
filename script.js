dc77493af80d629af0dce3dc2563a13f8faf38dc
infinity12020 <dilshodov.abduvali@mail.ru>
2023-12-21 11:40:23 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const colors = ["red" , "green"  , "blue" , "yellow" ]
+
+
+for ( let total = 0 ; total <= colors ; total++) {
+ console.log(total);
+
+}
+
+
+let n = 1
+
+do {
+   
+   console.log(n * n);
+    n++
+} while (n < 10 );
+
+for (let arr = 0 ; arr < 20 ; arr++) {
+if (arr % 2 !==0 ) {
+    console.log(arr);
+}
+
+
+}
